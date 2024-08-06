965150403a0e4950befa423e8dd282775ae488f5
cruzalexd <109928324+cruzalexd@users.noreply.github.com>
2024-08-05 21:17:56 -0300

    Add files via upload

---
+++ js/prueba.js
@@ -0,0 +1,7 @@
+//Esto se hizo para ver si funcionaba
+$('#aBailar').on('click', function(){
+    $('#aBailar').css('color', 'red');
+    $('body').css('background-color','red');
+    $('body').css('background-size','contain');
+}
+);
